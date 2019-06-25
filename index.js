da6fffafa3f3f3a037da36770e2202576ca06002
eddsonblanco <eddsonrbr@gmail.com>
2019-06-25 00:25:58 -0700

    routes ready and server config ready

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path')
 
 //initializations
 const app = express();
 
 //settings
 app.set( 'port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./src/lib/handlebars')  
+}));
+app.set('view engine', '.hbs');
 
 //Middleware
-// app.use(morga('dev'));
-
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //global variables
+app.use((req,res,next) =>{
+    next();
+})
 
 
 
 // routes
+app.use(require('./src/routes/index'));
 
 
 
